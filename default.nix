dadc00864555e551b1c1cffea33fa2d3cf78b2d7
Bruno Rodrigues <brodriguesco@protonmail.com>
2025-10-16 16:14:59 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,26 +1,18 @@
 let
- pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2025-02-17.tar.gz") {};
-    rix = (pkgs.rPackages.buildRPackage {
-      name = "rix";
-      src = pkgs.fetchgit {
-        url = "https://github.com/ropensci/rix/";
-        rev = "6b318795cbc51ce6d9d2dea714bec6bdde04e649";
-        sha256 = "sha256-DWS/ws4O4L1vlAfIghmPmww4hoXxURYk/N6Hj4rqKl8=";
-      };
-      propagatedBuildInputs = builtins.attrValues {
-        inherit (pkgs.rPackages) 
-          codetools
-          curl
-          jsonlite
-          sys;
-      };
-    });
+ pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2025-10-14.tar.gz") {};
+
+ rpkgs = builtins.attrValues {
+  inherit (pkgs.rPackages) rix rixpress;
+ };
+
   tex = (pkgs.texlive.combine {
   inherit (pkgs.texlive) scheme-small amsmath booktabs setspace lineno cochineal tex-gyre framed multirow wrapfig fontawesome5 tcolorbox orcidlink environ tikzfill pdfcol;
 });
+
  system_packages = builtins.attrValues {
   inherit (pkgs) R glibcLocalesUtf8 quarto nix;
 };
+
   in
   pkgs.mkShell {
     LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
@@ -31,6 +23,6 @@ let
     LC_PAPER = "en_US.UTF-8";
     LC_MEASUREMENT = "en_US.UTF-8";
 
-    buildInputs = [ tex system_packages rix ];
+    buildInputs = [ tex system_packages rpkgs ];
       
   }
